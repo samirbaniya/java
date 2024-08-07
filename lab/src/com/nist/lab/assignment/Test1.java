e167c092302c3d356ba6b4692ae88b484b909308
unknown <samirbaniya500@gmail.com>
2024-08-07 20:25:44 +0545

    pattern

--- lab/src/com/nist/lab/assignment/Test1.java
+++ lab/src/com/nist/lab/assignment/Test1.java
@@ -2,6 +2,112 @@ package com.nist.lab.assignment;
 
 public class Test1 {
 public static void main(String[] args) {
-	System.out.println("Enter age");
+//	1 2 3 4 5
+//	2 3 4 5
+//	3 4 5
+//	4 5
+//	5
+//	
+//	for(int i=1;i<=5;i++) {
+//		for(int j=i;j<=5;j++) {
+//			System.out.print(j);
+//		}
+//		System.out.println();
+//	}
+
+//	5
+//	4 5
+//	3 4 5
+//	2 3 4 5
+//	1 2 3 4 5
+	
+//	for(int i=5;i>=1;i--) {
+//		for(int j=i;j<=5;j++) {
+//			System.out.print(j);
+//		}
+//		System.out.println();
+//	}
+
+//	5 4 3 2 1
+//	4 3 2 1
+//	3 2 1
+//	2 1
+//	1
+
+//	for(int i=5;i>=1;i--) {
+//		for(int j=i;j>=1;j--) {
+//			System.out.print(j);
+//			}
+//			System.out.println();
+//	}
+	
+//	1
+//	2 1
+//	3 2 1
+//	4 3 2 1
+//	5 4 3 2 1
+	
+//	for(int i=1;i<=5;i++) {
+//		for(int j=i;j>=1;j--) {
+//			System.out.print(j);
+//			}
+//			System.out.println();
+//	}
+	
+//	1 2 3 4 5
+//	1 2 3 4
+//	1 2 3
+//	1 2
+//	1
+//	
+//	for(int i=5;i>=1;i--) {
+//		for(int j=1;j<=i;j++) {
+//			System.out.print(j);
+//		}
+//		System.out.println();
+//	}
+	
+	
+//	1
+//	1 2
+//	1 2 3
+//	1 2 3 4
+//	1 2 3 4 5
+	
+//	for(int i=1;i<=5;i++) {
+//		for(int j=1;j<=i;j++) {
+//				System.out.print(j);
+//			}
+//				System.out.println();
+//		}
+	
+//	5 4 3 2 1
+//	5 4 3 2
+//	5 4 3
+//	5 4
+//	5
+	
+//	for(int i=1;i<=5;i++) {
+//		for(int j=5;j>=i;j--) {
+//			System.out.print(j);
+//		}
+//		System.out.println();
+//	}
+	
+//	5
+//	5 4
+//	5 4 3
+//	5 4 3 2
+//	5 4 3 2 1
+	
+//	for(int i=5;i>=1;i--) {
+//		for(int j=5;j>=i;j--) {
+//			System.out.print(j);
+//		}
+//		System.out.println();
+//
+//	}
+	
+
 }
 }
